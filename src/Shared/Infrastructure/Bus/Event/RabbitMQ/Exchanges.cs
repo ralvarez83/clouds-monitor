749f536f62f182e4c630c37d0a35746d40aef570
Rubén Álvarez <rubenag83@gmail.com>
2024-09-03 10:39:23 +0200

    Faltaba quitar estas clases del commit anterior

--- src/Shared/Infrastructure/Bus/Event/RabbitMQ/Exchanges.cs
+++
@@ -1,4 +0,0 @@
-namespace Shared.Infrastructure.Bus.Event.RabbitMQ
-{
-  public record Exchanges(string Name, Subscribers[] Subscribers);
-}
--- src/Shared/Infrastructure/Bus/Event/RabbitMQ/Subscribers.cs
+++
@@ -1,4 +0,0 @@
-namespace Shared.Infrastructure.Bus.Event.RabbitMQ
-{
-  public record Subscribers(string QueueName, string EventName, ushort PrefetchCount = 10);
-}
